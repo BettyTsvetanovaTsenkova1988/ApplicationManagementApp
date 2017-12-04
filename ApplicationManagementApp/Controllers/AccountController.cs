e4f7e94ea21c3a758c27878ecf0a57ec5435e5b8
BettyTsvetanovaTsenkova1988 <bety_1988@abv.bg>
2017-12-04 10:45:53 +0300

    Update AccountController.cs

--- ApplicationManagementApp/Controllers/AccountController.cs
+++ ApplicationManagementApp/Controllers/AccountController.cs
@@ -157,7 +157,11 @@ namespace ApplicationManagementApp.Controllers
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
-                    await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
+             
+             
+             this.UserManager.AddToRole(user.Id, "viewer");
+             this.service.CreateCustomer(user);
+             await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
                     
                     // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
                     // Send an email with this link
